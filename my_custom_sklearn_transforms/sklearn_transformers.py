ad476878dec959fa3803147660aae92ba58af33d
Ricardo Gobbo <rg.aguas@gmail.com>
2020-08-27 02:37:22 -0300

    feat: add EncodeColumns transformer

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +15,22 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class EncodeColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        encoder = LabelEncoder()
+
+        for var in self.columns:
+            if data[var].dtype == 'object':
+                data[var] = encoder.fit_transform(data[var])
+        # Retornamos um novo dataframe sem as colunas alteradas
+        return data
